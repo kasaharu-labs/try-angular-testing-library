0975dd43a7b57a2bd92acda64a5756b74f7ecf0e
Wataru.Kasahara <kasahara1019@gmail.com>
2022-12-16 21:00:17 +0900

    feat: フォームの作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyFormPageComponent } from './features/my-form/pages/my-form/my-form.component';
 import { LyMyFormComponent } from './features/my-form/components/ly-my-form/ly-my-form.component';
+import { MyFormPageComponent } from './features/my-form/pages/my-form/my-form.component';
 
 @NgModule({
   declarations: [AppComponent, MyFormPageComponent, LyMyFormComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSelectModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/features/my-form/components/ly-my-form/ly-my-form.component.ts
+++ src/app/features/my-form/components/ly-my-form/ly-my-form.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+
+interface User {
+  firstName: string;
+  lastName: string;
+  address: {
+    prefectureId: string;
+  };
+  favaritCategoryIds: string[];
+}
 
 @Component({
   selector: 'app-ly-my-form',
   templateUrl: './ly-my-form.component.html',
-  styleUrls: ['./ly-my-form.component.scss']
+  styleUrls: ['./ly-my-form.component.scss'],
 })
 export class LyMyFormComponent {
-
+  form = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      prefectureId: new FormControl(''),
+    }),
+    favaritCategoryIds: new FormArray([]),
+  });
 }
